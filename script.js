6cb89d6db652d952c90b854232defb00b55497c5
unknown <you@example.com>
2018-01-13 16:48:39 +0100

    review6

---
+++ script.js
@@ -0,0 +1,127 @@
+var media = [
+  	{
+		title: 'John',
+		author: 'Doe',
+		genre: ["fsfs fsfs", "dad lkn", "bal bla"],
+		publisher: 18+"+",
+		media: 'CD',
+	  
+	},
+
+		{
+		title: 'John',
+		author: 'Doe',
+		genre: ["fsfs fsfs", "dad lkn", "bal bla"],
+		publisher: 18+"+",
+		media: 'Book',
+	  
+	},
+
+		{
+		title: 'John',
+		author: 'Doe',
+		genre: ["fsfs fsfs", "dad lkn", "bal bla"],
+		publisher: 18+"+",
+		media: 'DVD',
+	  
+	},
+		{
+		title: 'John',
+		author: 'Doe',
+		genre: ["fsfs fsfs", "dad lkn", "bal bla"],
+		publisher: 18+"+",
+		media: 'Comic',
+	  
+	},
+
+
+
+		{
+		title: 'John',
+		author: 'Doe',
+		genre: ["fsfs fsfs", "dad lkn", "bal bla"],
+		publisher: 18+"+",
+		media: 'Book',
+	  
+	},
+
+		{
+		title: 'John',
+		author: 'Doe',
+		genre: ["fsfs fsfs", "dad lkn", "bal bla"],
+		publisher: 18+"+",
+		media: 'DVD',
+	  
+	},
+
+
+		{
+		title: 'John',
+		author: 'Doe',
+		genre: ["fsfs fsfs", "dad lkn", "bal bla"],
+		publisher: 18+"+",
+		media: 'Comic',
+	  
+	},
+
+		{
+		title: 'John',
+		author: 'Doe',
+		genre: ["fsfs fsfs", "dad lkn", "bal bla"],
+		publisher: 18+"+",
+		media: 'Book',
+	  
+	}
+
+	
+];
+
+$("#title1").append("Title: " + media[0].title);
+$("#author1").append("Author: " + media[0].author);
+$("#genre1").append("Genre: " + media[0].genre);
+$("#media1").append("Media: " + media[0].media);
+
+
+
+$("#title2").append("Title: " + media[1].title);
+$("#author2").append("Author: " + media[1].author);
+$("#genre2").append("Genre: " + media[1].genre);
+$("#media2").append("Media: " + media[1].media);
+
+$("#title3").append("Title: " + media[2].title);
+$("#author3").append("Author: " + media[2].author);
+$("#genre3").append("Genre: " + media[2].genre);
+$("#media3").append("Media: " + media[2].media);
+
+$("#title4").append("Title: " + media[3].title);
+$("#author4").append("Author: " + media[3].author);
+$("#genre4").append("Genre: " + media[3].genre);
+$("#media4").append("Media: " + media[3].media);
+
+$("#title5").append("Title: " + media[4].title);
+$("#author5").append("Author: " + media[4].author);
+$("#genre5").append("Genre: " + media[4].genre);
+$("#media5").append("Media: " + media[4].media);
+
+
+$("#title6").append("Title: " + media[5].title);
+$("#author6").append("Author: " + media[5].author);
+$("#genre6").append("Genre: " + media[5].genre);
+$("#media6").append("Media: " + media[5].media);
+
+$("#title7").append("Title: " + media[6].title);
+$("#author7").append("Author: " + media[6].author);
+$("#genre7").append("Genre: " + media[6].genre);
+$("#media7").append("Media: " + media[6].media);
+
+
+$("#title8").append("Title: " + media[7].title);
+$("#author8").append("Author: " + media[7].author);
+$("#genre8").append("Genre: " + media[7].genre);
+$("#media8").append("Media: " + media[7].media);
+
+
+
+
+
+
